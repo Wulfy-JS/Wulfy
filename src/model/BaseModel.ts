d8da4944b71d6081de5ac3e379eea7a53052ebb6
80LK <80lkru@gmail.com>
2021-11-23 21:21:22 +0300

    add static methods belongsTo(Many)/hasOne/hasMany in BaseModel

--- src/model/BaseModel.ts
+++ src/model/BaseModel.ts
@@ -1,12 +1,40 @@
-import sequelize, { ModelStatic, ModelAttributes, InitOptions, Sequelize } from "sequelize";
+import sequelize, { ModelStatic, ModelAttributes, InitOptions, Sequelize, BelongsToOptions, BelongsToManyOptions, HasOneOptions, HasManyOptions } from "sequelize";
 const Model = sequelize.Model;
+type Model<A extends {} = any, B extends {} = A> = sequelize.Model<A, B>;
+type BelongsTo<S extends Model = Model, T extends Model = Model> = sequelize.BelongsTo<S, T>;
+type BelongsToMany<S extends Model = Model, T extends Model = Model> = sequelize.BelongsToMany<S, T>;
+type HasOne<S extends Model = Model, T extends Model = Model> = sequelize.HasOne<S, T>;
+type HasMany<S extends Model = Model, T extends Model = Model> = sequelize.HasMany<S, T>;
+
+
+interface SetupData<T extends Model<any, any> = any> {
+	init?: {
+		attributes: ModelAttributes;
+		options: Partial<InitOptions>;
+	};
+	belongsTo?: {
+		target: ModelStatic<T>;
+		options?: BelongsToOptions;
+	};
+	belongsToMany?: {
+		target: ModelStatic<T>;
+		options?: BelongsToManyOptions;
+	};
+	hasOne?: {
+		target: ModelStatic<T>;
+		options?: HasOneOptions;
+	};
+	hasMany?: {
+		target: ModelStatic<T>;
+		options?: HasManyOptions;
+	};
+}
 
 abstract class BaseModel<TModelAttributes extends {} = any, TCreationAttributes extends {} = TModelAttributes>
 	extends Model<TModelAttributes, TCreationAttributes>
 {
 	private static _sequelize: Sequelize;
-	private static _attributes: ModelAttributes;
-	private static _options: Partial<InitOptions>;
+	private static _setupData: SetupData = {};
 	private static models: (typeof BaseModel)[] = [];
 
 	public static init<MS extends ModelStatic<BaseModel>, M extends InstanceType<MS>>(
@@ -16,23 +44,115 @@ abstract class BaseModel<TModelAttributes extends {} = any, TCreationAttributes 
 	public static init<MS extends ModelStatic<BaseModel>, M extends InstanceType<MS>>(
 		attributes: ModelAttributes<M, M['_attributes']>, options: Partial<InitOptions<M>> = {}
 	) {
-		this._attributes = attributes;
-		this._options = options;
+		this._setupData.init = { attributes, options };
 
 		if (this._sequelize)
-			this.initModel()
+			this._init()
 		else
 			BaseModel.models.push(this);
 
 		return this;
 	}
-	private static initModel() {
-		Model.init.bind(this)(this._attributes, { ...this._options, sequelize: this._sequelize });
+
+	private static _init() {
+		if (this._setupData.init)
+			Model.init.bind(this)(this._setupData.init.attributes, { ...this._setupData.init.options, sequelize: this._sequelize });
 	}
+
+	public static belongsTo<M extends Model, T extends Model>(
+		this: ModelStatic<M>, target: ModelStatic<T>, options?: BelongsToOptions
+	): BelongsTo<M, T>;
+	public static belongsTo<M extends Model, T extends Model>(
+		target: ModelStatic<T>, options?: BelongsToOptions
+	): BelongsTo<M, T> {
+		this._setupData.belongsTo = { target, options };
+
+		if (this._sequelize)
+			return this._belongsTo()
+
+		BaseModel.models.push(this);
+		return
+	}
+	private static _belongsTo() {
+		if (this._setupData.belongsTo)
+			return Model.belongsTo.bind(this)(this._setupData.belongsTo.target, this._setupData.belongsTo.options);
+	}
+
+
+	public static belongsToMany<M extends Model, T extends Model>(
+		this: ModelStatic<M>, target: ModelStatic<T>, options: BelongsToManyOptions
+	): BelongsToMany<M, T>;
+	public static belongsToMany<M extends Model, T extends Model>(
+		target: ModelStatic<T>, options: BelongsToManyOptions
+	): BelongsToMany<M, T> {
+		this._setupData.belongsToMany = { target, options };
+
+		if (this._sequelize)
+			return this._belongsToMany()
+
+		BaseModel.models.push(this);
+		return
+	}
+
+	private static _belongsToMany() {
+		if (this._setupData.belongsToMany)
+			return Model.belongsToMany.bind(this)(this._setupData.belongsToMany.target, this._setupData.belongsToMany.options);
+	}
+
+	//hasMany
+	//hasOne
+	//belongsToMany
+
+	public static hasOne<M extends Model, T extends Model>(
+		this: ModelStatic<M>, target: ModelStatic<T>, options?: HasOneOptions
+	): HasOne<M, T>;
+	public static hasOne<M extends Model, T extends Model>(
+		target: ModelStatic<T>, options?: HasOneOptions
+	): HasOne<M, T> {
+		this._setupData.hasOne = { target, options };
+
+		if (this._sequelize)
+			return this._hasOne()
+
+		BaseModel.models.push(this);
+		return
+	}
+	private static _hasOne() {
+		if (this._setupData.hasOne)
+			return Model.hasOne.bind(this)(this._setupData.hasOne.target, this._setupData.hasOne.options);
+	}
+	public static hasMany<M extends Model, T extends Model>(
+		this: ModelStatic<M>, target: ModelStatic<T>, options?: HasManyOptions
+	): HasMany<M, T>;
+	public static hasMany<M extends Model, T extends Model>(
+		target: ModelStatic<T>, options?: HasManyOptions
+	): HasMany<M, T> {
+		this._setupData.hasMany = { target, options };
+
+		if (this._sequelize)
+			return this._hasMany()
+
+		BaseModel.models.push(this);
+		return
+	}
+	private static _hasMany() {
+		if (this._setupData.hasMany)
+			return Model.hasMany.bind(this)(this._setupData.hasMany.target, this._setupData.hasMany.options);
+	}
+
+
 
 	public static setup(sequelize: Sequelize) {
 		this._sequelize = sequelize;
-		this.models.forEach(e => e.initModel());
+		this.models.forEach(e => e._setup());
+	}
+
+	private static _setup() {
+		this._init();
+		this._belongsTo();
+		this._belongsToMany();
+		this._hasOne();
+		this._hasMany();
 	}
 }
 
