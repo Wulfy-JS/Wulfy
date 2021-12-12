7e741b476390c91e6af669e79df8deacf0bcfc40
80LK <80lkru@gmail.com>
2021-12-12 21:29:34 +0300

    support win and linux

--- src/core/Core.ts
+++ src/core/Core.ts
@@ -28,7 +28,7 @@ interface DataBaseConfig {
 }
 abstract class Core {
 	public readonly projectFolder = process.cwd();
-	protected readonly moduleFolder = normalize(import.meta.url.slice(7) + "/../../../");
+	protected readonly moduleFolder = normalize(import.meta.url.slice(process.platform == "win32" ? 8 : 7) + "/../../../");
 	public readonly config = new Config();
 
 	protected readonly routes = new RouteMap();
