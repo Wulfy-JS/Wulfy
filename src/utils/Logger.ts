cdf38eda0dd86e8ab7499ced4e58883945cf65ca
80LK <80LK@users.noreply.github.com>
2022-02-09 15:21:37 +0300

    extend logger

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -3,9 +3,19 @@ import DotEnv from "./DotEnv";
 
 const Logger = (() => {
 	DotEnv.init();
-	return pino({
+	const logger = pino({
 		level: process.env.MODE == "dev" ? "debug" : "info"
 	});
+	process.on('uncaughtException', pino.final(logger, (err, finalLogger) => {
+		finalLogger.error(err, 'uncaughtException')
+		process.exit(1)
+	}))
+
+	process.on('unhandledRejection', pino.final(logger, (err, finalLogger) => {
+		finalLogger.error(err, 'unhandledRejection')
+		process.exit(1)
+	}))
+	return logger;
 })()
 
 export default Logger;
